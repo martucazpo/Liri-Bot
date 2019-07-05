6b4811d20130b023c9d8745b1043d296b604079e
MarcoTucsonpo <martucazpo@gmail.com>
2019-07-04 18:00:48 -0700

    working on movie-this

--- liri.js
+++ liri.js
@@ -1,15 +1,80 @@
 
-require("dotenv").config();
+//require("dotenv").config();
 
-var keys = require("./keys.js");
+//var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
 
 
-* `concert-this`
+var action = process.argv[2];
 
-* `spotify-this-song`
+var nodeArgs = process.argv;
 
-* `movie-this`
 
-* `do-what-it-says`
+switch (action){
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "default":
+    break;
+}
+
+
+// `concert-this`
+
+// `spotify-this-song`
+
+// `movie-this`
+function movieThis() {
+
+    var movieName = "";
+
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else {
+            movieName += nodeArgs[i];
+
+        }
+
+        console.log(movieName);
+    }
+
+  /*  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function (response) {
+            var respond = response.data;
+            console.log(respond);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
+*/
+};
+
+// `do-what-it-says`
